992c20d907b942154b644b19c3595f5f3b580073
Dunklas <dunklas@gmail.com>
2024-04-09 22:14:14 +0200

    Add support for dark mode (#9)
    Including a new image of David.

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,31 @@
-import { Container } from "@mui/material";
+import {
+  Container,
+  CssBaseline,
+  ThemeProvider,
+  createTheme,
+  useMediaQuery,
+  responsiveFontSizes
+} from "@mui/material";
 
 import AskDavid from "./pages/AskDavid";
+import { useMemo } from "react";
 
 function App() {
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+
+  const userTheme = useMemo(() => responsiveFontSizes(createTheme({
+    palette: {
+      mode: prefersDarkMode ? 'dark' : 'light'
+    }
+  })), [prefersDarkMode]);
+
   return (
-    <Container sx={{ padding: 2 }}>
-      <AskDavid />
-    </Container>
+    <ThemeProvider theme={userTheme}>
+      <CssBaseline/>
+      <Container sx={{ padding: 2 }}>
+        <AskDavid />
+      </Container>
+    </ThemeProvider>
   );
 }
 
