4b24a26487bfe719d96139ce9f3989f1292288da
Rickard Andersson <dunklas@gmail.com>
2024-04-11 22:43:33 +0200

    Improve about text

--- web/src/pages/WhoIsDavid.tsx
+++ web/src/pages/WhoIsDavid.tsx
@@ -8,17 +8,16 @@ const WhoIsDavid = () => (
     </Typography>
     <David />
     <Typography>
-      David is an extraordinary individual with a zest for life! He's a
-      connoisseur of craft beer, a tech enthusiast who loves investing in the
-      latest computer parts, a guitar aficionado, and an ardent animal lover
-      with a special place in his heart for dogs.
+      David is truly an awesome guy with a diverse set of passions.
+      He appreciates a good craft beer, is keen on keeping up with the latest in technology—especially when it comes to computer parts—and he has a deep appreciation for music, evident in his love for guitars.
+      Above all, he's a devoted animal lover, with dogs holding a special place in his heart.
     </Typography>
     <Typography>
-      However, David faces a challenge that many of us encounter: decision
-      making. It's not that he's incapable of making decisions; rather, he tends
-      to avoid making them altogether. This website is dedicated to helping
-      David (and anyone else who finds themselves in a similar predicament)
-      conquer this hurdle.
+      However, David faces a challenge that many of us encounter: decision making.
+      It's not that he's incapable of making decisions; rather, he tends to avoid making them altogether. 
+    </Typography>
+    <Typography>
+      This website is dedicated to helping David (and anyone else who finds themselves in a similar predicament) conquer this hurdle.
     </Typography>
   </Stack>
 );
