39571e5462ecf3f65dca5ce4d8c7ef43c286ec8e
Rickard Andersson <dunklas@gmail.com>
2024-04-12 00:02:38 +0200

    Add eslint

--- web/eslint.config.js
+++ web/eslint.config.js
@@ -1,11 +1,23 @@
 // @ts-check
 import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
+import reactRefresh from "eslint-plugin-react-refresh";
 
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
   {
-    ignores: ["dist"],
+    languageOptions: {
+      ecmaVersion: 11,
+    },
+    plugins: {
+      "react-refresh": reactRefresh,
+    },
+    rules: {
+      "react-refresh/only-export-components": "warn",
+    },
+  },
+  {
+    ignores: ['dist', 'eslint.config.js']
   }
 );
